d11660edda3ad7a3450266694c74887bb265b7ed
andr984g <andreas@airtilheandreas.students.clients.local>
2024-04-25 11:07:11 +0200

    Tested more ConvNet and LinNet

--- src/utils/build_model.py
+++ src/utils/build_model.py
@@ -4,10 +4,11 @@ def build_model(model_type: str, CFG, device: str):
     H = CFG['data']['H']
     W = CFG['data']['W']
     latent_dim = CFG['AE']['D']
-    N_middle_latent = CFG['AE']['N_middle_latent']
+    N_latent_1 = CFG['AE']['N_latent_1']
+    N_latent_2 = CFG['AE']['N_latent_2']
     if model_type == 'AutoEncoder':
-        encoder = Encoder(H, W, latent_dim, N_middle_latent)
-        decoder = Decoder(H, W, latent_dim, N_middle_latent)
+        encoder = Encoder(H, W, latent_dim, N_latent_1, N_latent_2)
+        decoder = Decoder(H, W, latent_dim, N_latent_1, N_latent_2)
         model = AE(encoder, decoder)
     else:
         raise ValueError(f"Unsupported model type: {model_type}")
