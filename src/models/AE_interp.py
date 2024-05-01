bf306685e4736d243647ae925063d6b408f4f245
oe_andreas <andreas.oestergaard20@gmail.com>
2024-05-01 16:35:42 +0200

    AE for interpolation

--- src/models/AE_interp.py
+++ src/models/AE_interp.py
@@ -94,9 +94,12 @@ class AE_interp(nn.Module):
 
     def loss(self, x: torch.tensor):
         z = self.encoder(x)
-        z_interp = (z[0:-2:1]+z[2::1])/2
-        z_interp = torch.cat((z[0:1], z_interp, z[-2:-1]))
+        #z_interp = (z[0:-2:1]+z[2::1])/2
+        #z_interp = torch.cat((z[0:1], z_interp, z[-2:-1]))
 
         x_recon = self.decoder(z)
-        x_recon_interp = self.decoder(z_interp)
-        return nn.functional.mse_loss(x_recon, x) + nn.functional.mse_loss(x_recon_interp,x)
+        #x_recon_interp = self.decoder(z_interp)
+        #return nn.functional.mse_loss(x_recon, x) #+ nn.functional.mse_loss(x_recon_interp,x[1:-1])
+
+        x_recon = self.forward(x)
+        return nn.functional.mse_loss(x_recon, x)
--- src/utils/build_model.py
+++ src/utils/build_model.py
@@ -1,6 +1,22 @@
-from src.models.AE_interp import Encoder, Decoder, AE_interp
+from src.models.AE import Encoder, Decoder, AE
+from src.models.AE_interp import AE_interp
+
 
 def build_model(model_type: str, CFG, device: str):
+    H = CFG['data']['H']
+    W = CFG['data']['W']
+    latent_dim = CFG['AE']['D']
+    N_latent_1 = CFG['AE']['N_latent_1']
+    N_latent_2 = CFG['AE']['N_latent_2']
+    if model_type == 'AutoEncoder':
+        encoder = Encoder(H, W, latent_dim, N_latent_1, N_latent_2)
+        decoder = Decoder(H, W, latent_dim, N_latent_1, N_latent_2)
+        model = AE(encoder, decoder)
+    else:
+        raise ValueError(f"Unsupported model type: {model_type}")
+    return model
+
+def build_model_interp(model_type: str, CFG, device: str):
     H = CFG['data']['H']
     W = CFG['data']['W']
     latent_dim = CFG['AE']['D']
